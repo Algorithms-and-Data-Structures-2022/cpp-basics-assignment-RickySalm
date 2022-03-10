5bef5ddad0920d4091d675ceeee6d26c94f6f553
Radmir Salimov <radmir-prog@mail.ru>
2022-03-10 22:06:00 +0300

    Салимов 11-109

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@
 using namespace std;
 using namespace assignment;
 
+int squared_diff(int left, int right) {
+  int total;
+  total = left - right;
+  total = total * total;
+  return total;
+}
+
 int main() {
 
   cout << "Write your code here...\n";
-
   return 0;
 }
